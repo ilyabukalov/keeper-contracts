465bfa31474b0f3e096fc18a27be90cc17bab63b
Ilia Bukalov <ilyabukalov@gmail.com>
2019-09-30 16:16:15 +0800

    Direct purchase smart contract

---
+++ contracts/DirectPurchase.sol
@@ -0,0 +1,44 @@
+pragma solidity 0.5.6;
+// Copyright BigchainDB GmbH and Ocean Protocol contributors
+// SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
+// Code is Apache-2.0 and docs are CC-BY-4.0
+
+import 'openzeppelin-eth/contracts/token/ERC20/IERC20.sol';
+
+/**
+ * @title Ocean Protocol DirectPurchase Contract
+ * @author Ilia Bukalov (ilyabukalov@gmail.com)
+ *
+ * @dev Implementation of the DirectPurchase.
+ *      IERC20 token is OceanToken.
+ */
+contract DirectPurchase {
+
+    IERC20 token;
+
+    event TokenSent(
+	address indexed _from,
+	address indexed _to,
+	uint256 _amount,
+        bytes32 _reference1,
+	bytes32 indexed _reference2
+    )
+
+    /**
+    * @dev DirectPurchase Initializer
+    *      Runs only on initial contract creation.
+    * @param _token refers to the OceanToken address
+    */
+    function initialize(IERC20 _token
+    )
+        public
+        initializer
+    {
+	token = _token;
+    }
+
+    function sendTokenAndLog(address to, uint256 amount, bytes32 reference1, bytes32 reference2) public {
+	token.transferFrom(msg.sender, to, amount);
+        emit TokenSent(msg.sender, to, amount, reference1, reference2);
+    }
+}
